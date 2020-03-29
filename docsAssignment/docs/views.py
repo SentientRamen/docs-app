d87d85e036b3178f07a1073c2162ca5d69f15c0c
SentientRamen <deadpool12497@gmail.com>
2020-03-29 19:55:28 +0530

    added login and register forms, used external templates for the forms and setup dashboard

---
+++ docsAssignment/docs/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import CreateUserForm
+
+
+# Create your views here.
+def register_page(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+    else:
+        form = CreateUserForm()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'Account was created for ' + user)
+
+                return redirect('login')
+
+        context = {'form': form}
+        return render(request, 'docs/register.html', context)
+
+
+def login_page(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, 'Username OR password is incorrect')
+
+        context = {}
+        return render(request, 'docs/login.html', context)
+
+
+def logout_page(request):
+    logout(request)
+    return redirect('login')
+
+
+@login_required(login_url='login')
+def home(request):
+    context = {}
+    return render(request, 'docs/dashboard.html', context)
