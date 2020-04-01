fdc8add090e8ce1184046cd3008f688325c89b41
SentientRamen <deadpool12497@gmail.com>
2020-04-01 13:39:52 +0530

    added ui for online tracking

--- docsAssignment/docs/consumers.py
+++ docsAssignment/docs/consumers.py
@@ -2,18 +2,31 @@
 import json
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from .models import Authorized, Documents
 
 
 class DocConsumer(WebsocketConsumer):
-    def connect(self):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'doc_%s' % self.room_name
 
+    def connect(self):
         # Join room group
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
         )
+        #
+        # message = self.get_viewing_history()
+        # async_to_sync(self.channel_layer.group_send)(
+        #     self.room_group_name,
+        #     {
+        #         'type': 'chat_message',
+        #         'message': message
+        #     }
+        # )
 
         self.accept()
 
@@ -46,3 +59,12 @@ class DocConsumer(WebsocketConsumer):
         self.send(text_data=json.dumps({
             'message': message
         }))
+
+    def get_viewing_history(self):
+        document = Documents.objects.get(name=self.room_name)
+        viewing_history = Authorized.objects.filter(document=document)
+        view_history_data = {}
+        for i in viewing_history:
+            view_history_data[i.user.username] = i.last_visited.strftime('%Y-%m-%d %H:%M')
+
+        return view_history_data
