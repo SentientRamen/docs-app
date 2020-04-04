5b5854624f134a3638d18603023a954595a161d8
SentientRamen <deadpool12497@gmail.com>
2020-04-04 16:53:56 +0530

    updated room frontend tweaks

--- docsAssignment/docs/static/docs/js/room.js
+++ docsAssignment/docs/static/docs/js/room.js
@@ -140,5 +140,5 @@ function removeIdle() {
 function random_rgba() {
     // Random colour generator
     var o = Math.round, r = Math.random, s = 255;
-    return 'rgba(' + o(r() * s) + ',' + o(r() * s) + ',' + o(r() * s) + ',' + r().toFixed(1) + ')';
+    return 'rgba(' + o(r() * s) + ',' + o(r() * s) + ',' + o(r() * s) + ',' + 0.6 + ')';
 }
